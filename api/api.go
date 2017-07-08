d03e0c24b2207df3c5857133f0e273fda22b2cd1
adolphlwq <kenan3015@gmail.com>
2017-07-08 17:09:15 +0800

    add fake data for frontend test

--- api/api.go
+++ api/api.go
@@ -1,15 +1,17 @@
 package api
 
 import (
+	crand "crypto/rand"
 	"net/http"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
 )
 
 func FlippedServer() {
 	r := gin.Default()
 	r.GET("/health", health)
-
+	r.GET("/fake", fake)
 	r.Run(":9090")
 }
 
@@ -17,4 +19,13 @@ func health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "health",
 	})
+}
+
+func fake(c *gin.Context) {
+	b := make([]byte, 1)
+	_, err := crand.Read(b)
+	if err != nil {
+		logrus.Fatalf("generate fake data error: %v", err)
+	}
+	c.JSON(http.StatusOK, gin.H{"data": b[0]})
 }
